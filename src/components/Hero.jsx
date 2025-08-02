47556c5d4acbff8febadeefe8de5d5bed9dbb9cb
srishylam burla <srishylamburla1@gmail.com>
2025-08-02 16:59:45 +0530

    some files added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,10 +10,14 @@ const Hero = () => {
         , Full Stack Developer</h1>
         <p className='text-lg mb-6 text-gray-400'>I specialize in building modern and responsive web applications</p>
         <div className='space-x-4'>
+            <a href='#contact'>
             <button className='btn btn-block text-white bg-gradient-to-r
              from-green-400 to-blue-500 px-4 py-2 rounded-full transform transition-transform duration-300 hover:scale-105'>Contact Me</button>
+             </a>
+             <a href="/Srishylam-Resume.pdf" target="_blank" rel="noopener noreferrer">
             <button className='btn btn-block text-white bg-gradient-to-r
              from-pink-400 to-yellow-500 px-4 py-2 rounded-full transform transition-transform duration-300 hover:scale-105'>Resume</button>
+             </a>
         </div>
     </div>
   )
