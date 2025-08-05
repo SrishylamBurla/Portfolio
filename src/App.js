6b4a65b979b6736e43aa7dd52d2b3e6da001f291
srishylam burla <srishylamburla1@gmail.com>
2025-08-05 21:02:38 +0530

    updated animation

--- src/App.js
+++ src/App.js
@@ -4,11 +4,27 @@ import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
- import { ToastContainer } from 'react-toastify';
+import { ToastContainer } from "react-toastify";
+import { motion } from "framer-motion";
+import { useScroll } from "framer-motion";
 
 function App() {
+  const { scrollYProgress } = useScroll();
   return (
     <div>
+      <motion.div
+        style={{
+          scaleX: scrollYProgress,
+          position: "fixed",
+          top: 0,
+          left: 0,
+          right: 0,
+          height: 5,
+          originX: 0,
+          zIndex: 1,
+          backgroundImage: "linear-gradient(to right, #f87171, #facc15)",
+        }}
+      />
       <Navbar />
       <Hero />
       <About />
