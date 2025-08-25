a576b44ba3b23580634af3c4bc9b65c555ad9487
srishylam burla <srishylamburla1@gmail.com>
2025-08-25 12:04:03 +0530

    updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { FaFacebook, FaGithub, FaInstagram, FaWhatsapp } from "react-icons/fa";
 const Footer = () => {
   return (
     <footer className="bg-black text-white">
-      <div className="container mx-auto px-8 md:px-16 lg:px-24">
+      <div className="container mx-auto px-4 md:px-16 lg:px-24">
         <div className="flex flex-col md:flex-row items-items-start space-y-6 py-10">
           <div className="flex-1 px-4">
             <h1 className="text-sm font-bold text-gray-200">Srishylam</h1>
@@ -21,7 +21,7 @@ const Footer = () => {
               />
               <button
                 className="text-white bg-gradient-to-r from-green-400 to-blue-500 
-              hover:scale-105 px-8 py-2 rounded-r-lg transition-transform"
+              hover:scale-105 px-4 py-2 md:px-8 rounded-r-lg transition-transform"
               >
                 Subscribe
               </button>
